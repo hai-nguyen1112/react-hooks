33e8fa0958df19cd666e11bf95ffb3a3896ae077
Hai Nguyen <hainguyen@ip-10-1-9-98.ec2.internal>
2020-02-27 15:03:30 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useCallback, useMemo, useEffect} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [memoCount, setMemoCount] = useState(0)
+
+    const [callbackCount, setCallbackCount] = useState(0)
+
+    const callBackFunction = useCallback(() => {
+        console.log(callbackCount, "callback called")
+        return callbackCount
+    }, [callbackCount])
+
+    const memoFunction = () => {
+        console.log(memoCount, "memo called")
+    }
+
+    useMemo(memoFunction, [memoCount])
+
+    return (
+        <div className="App">
+            <ChildComponent action={callBackFunction}/>
+            <button onClick={() => setCallbackCount(callbackCount + 1)}>
+                Change callback count
+            </button>
+            <button onClick={() => setMemoCount(memoCount + 1)}>
+                Change memo count
+            </button>
+        </div>
+    );
 }
+
+const ChildComponent = ({action}) => {
+    const [value, setValue] = useState(0)
+
+    useEffect(() => {
+        let val = action()
+        setValue(val)
+    }, [action])
+
+    return (
+        <>
+            Child: {value}
+        </>
+    )
+};
 
 export default App;
